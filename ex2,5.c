929309185ef73dc89deb310abf142fc8f6560f95
huydt84 <77562200+huydt84@users.noreply.github.com>
2021-03-29 10:11:28 +0700

    Add files via upload

---
+++ ex2,5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main()
+{
+printf("*************************\n");
+printf("My name is Binh Nguyen.\n");
+printf("Nice to meet you.\n");
+printf("Hope you will have happy time\n");
+printf("*************************");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
