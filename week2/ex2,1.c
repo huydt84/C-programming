25f87fbf032740c85674a7efddaba12120870de4
Đinh Trọng Huy <77562200+huydt84@users.noreply.github.com>
2021-04-12 11:21:34 +0700

    Add files via upload

---
+++ week2/ex2,1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	printf("Welcome to C Programming Introduction.\n");
+	return 0;
+}
+
---
+++ week2/ex2,3.c
@@ -0,0 +1,8 @@
+/* Your name – your class */
+/* This is my second program in C */
+#include <stdio.h>
+main()
+{
+printf("Welcome to C");
+printf("Programming Introduction.\n");
+}
